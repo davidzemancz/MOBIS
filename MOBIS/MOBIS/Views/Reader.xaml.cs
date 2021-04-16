472fb5d77c83849cd34d33e34dc5062f9866b6bd
kwimbik <kwimbiks@gmail.com>
2021-04-16 19:13:46 +0200

    Create Reader.xaml.cs

---
+++ MOBIS/MOBIS/Views/Reader.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOBIS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reader : ContentPage
+    {
+        public Reader()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.UcetViewModel();
+            Zprava.Text = " ";
+        }
+    }
+}
