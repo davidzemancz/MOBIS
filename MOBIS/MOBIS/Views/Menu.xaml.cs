2037b5d35843b267305bc4a981960f31833a11b2
kwimbik <kwimbiks@gmail.com>
2021-04-16 13:49:16 +0200

    Create Menu.xaml.cs

---
+++ MOBIS/MOBIS/Views/Menu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOBIS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : TabbedPage
+    {
+        public Menu()
+        {
+            NavigationPage ucet = new NavigationPage(new Ucet());
+            ucet.IconImageSource = "schedule.png";
+            ucet.Title = "Ucet";
+
+            NavigationPage informace = new NavigationPage(new Novinky());
+            informace.IconImageSource = "schedule.png";
+            informace.Title = "Novinky";
+
+            NavigationPage novinky = new NavigationPage(new Informace());
+            novinky.IconImageSource = "schedule.png";
+            novinky.Title = "Informace";
+
+            Children.Add(informace);
+            Children.Add(novinky);
+            Children.Add(ucet);
+        }
+    }
+}
