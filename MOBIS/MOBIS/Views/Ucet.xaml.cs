1e4b8900c2afdeb0e2a667664cf7bd2558ed9eac
kwimbik <kwimbiks@gmail.com>
2021-04-16 13:49:08 +0200

    Update Ucet.xaml.cs

--- MOBIS/MOBIS/Views/Ucet.xaml.cs
+++ MOBIS/MOBIS/Views/Ucet.xaml.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MOBIS.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class View1 : ContentView
+    public partial class Ucet : ContentPage
     {
-        public View1()
+        public Ucet()
         {
             InitializeComponent();
+            this.BindingContext = new ViewModels.UcetViewModel();
         }
     }
 }
