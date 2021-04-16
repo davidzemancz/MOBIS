2620568b73e550e1c01850fa079bc7ee5f75b642
kwimbik <kwimbiks@gmail.com>
2021-04-16 18:12:41 +0200

    Update Ucet.xaml.cs

--- MOBIS/MOBIS/Views/Ucet.xaml.cs
+++ MOBIS/MOBIS/Views/Ucet.xaml.cs
@@ -15,6 +15,10 @@ namespace MOBIS.Views
         {
             InitializeComponent();
             this.BindingContext = new ViewModels.UcetViewModel();
+            YourLableID.Text = "ID: " +  Models.User.soucastny_user.Id.ToString();
+            YourLableEmail.Text = "Email: " + Models.User.soucastny_user.Email.ToString();
+            YourLablePracoviste.Text = "Pracoviste: " + Models.User.soucastny_user.Workplace.ToString();
+            YourLableRole.Text = "Pozice: " + Models.User.soucastny_user.Role.ToString();
         }
     }
 }
