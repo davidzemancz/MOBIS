10169888f842366004f4c631d9a2e84b917cd3f9
kwimbik <kwimbiks@gmail.com>
2021-04-16 12:13:40 +0200

    Update LoginPage.xaml.cs
    Login logika

--- MOBIS/MOBIS/Views/LoginPage.xaml.cs
+++ MOBIS/MOBIS/Views/LoginPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using MOBIS.ViewModels;
+﻿using MOBIS.API;
+using MOBIS.Models;
+using MOBIS.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,7 +23,31 @@ namespace MOBIS.Views
 
         void TryToLogin(object sender, EventArgs args)
         {
-             Heslo.Text = "YEEWAH";
+            
+            User user = new User("pepazdepa@mail.cz", "mamradmekac");
+            string jsonInData = JsonSerializer.Serialize(user);
+            string jsonOutData = RestApi.Post("user/login", jsonInData, out bool ok);
+            if (ok)
+            {
+                user = JsonSerializer.Deserialize<User>(jsonOutData);
+                if (user.Exists)
+                {
+                    // Uzivatel existuje
+                    Console.WriteLine("Id: " + user.Id);
+                    Console.WriteLine("Key: " + user.Key);
+                    Console.WriteLine("E-mail: " + user.Email);
+                    Navigation.PushAsync(new Zpravy());
+                }
+                else
+                {
+                    // Uzivatel neexistuje
+                    DisplayAlert("CHYBA?", "SPATNE JMENO NEBO HESLO", "OK");
+                }
+            }
+            else
+            {
+                throw new Exception("Připojení se nezdařilo.");
+            }
         }
     }
 }
