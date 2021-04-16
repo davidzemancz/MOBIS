ffb55a54f5e8e9a0f592e67b033d3c0bc6fd1aab
kwimbik <kwimbiks@gmail.com>
2021-04-16 12:44:25 +0200

    Create UcetViewModel.cs

---
+++ MOBIS/MOBIS/ViewModels/UcetViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MOBIS.ViewModels
+{
+    public class UcetViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+        public UcetViewModel()
+        { 
+        }
+    }
+}
