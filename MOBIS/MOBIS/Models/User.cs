81d644965d75f1fd124dcc2dfc69fde4144b4a56
kwimbik <kwimbiks@gmail.com>
2021-04-16 12:11:39 +0200

    Create User.cs
    user class

---
+++ MOBIS/MOBIS/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOBIS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public bool Exists { get; set; }
+
+        public string Key { get; set; }
+
+        public string Email { get; set; }
+
+        private string Password { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string email, string password)
+        {
+            this.Email = email;
+            this.Password = password;
+        }
+    }
+}
