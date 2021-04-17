3d1d6b8e571f4b28af34e346d007db06319c4965
kwimbik <kwimbiks@gmail.com>
2021-04-17 04:32:55 +0200

    Update Reader.xaml.cs
    nadpis + cas

--- MOBIS/MOBIS/Views/Reader.xaml.cs
+++ MOBIS/MOBIS/Views/Reader.xaml.cs
@@ -29,7 +29,7 @@ namespace MOBIS.Views
             string jsonOutData = RestApi.Post("content/load", jsonInData, out bool ok); 
             var data = JsonSerializer.Deserialize<Paper>(jsonOutData);
             Zprava.Text = data.ContentText;
-            SetBinding(ContentPage.TitleProperty, new Binding(data.Title));
+            Title = data.Date + " " + data.Title;
         }
     }
 }
