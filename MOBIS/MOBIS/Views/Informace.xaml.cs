b2b33e2b768fb70ac3f49d84b1a53b03460af9b0
kwimbik <kwimbiks@gmail.com>
2021-04-17 11:30:00 +0200

    Update Informace.xaml.cs

--- MOBIS/MOBIS/Views/Informace.xaml.cs
+++ MOBIS/MOBIS/Views/Informace.xaml.cs
@@ -27,7 +27,22 @@ namespace MOBIS.Views
             this.BindingContext = new ViewModels.InformaceViewModel();
 
             this.NovinkyListView.ItemsSource = this.Papers;
+        }
 
+        void cist_vice(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Reader(((Button)sender).CommandParameter.ToString()));
+        }
+
+        private void Edit(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Reader(((Button)sender).CommandParameter.ToString(), true));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.Papers.Clear();
             string jsonInData = "{ \"UserId\":" + User.Current.Id + ",\"Type\":\"Informace\"}"; //jednicka pro novinky, 2 pro informace
             string jsonOutData = RestApi.Post("content/list", jsonInData, out bool ok);
             var data = JsonSerializer.Deserialize<Paper[]>(jsonOutData);
@@ -36,11 +51,6 @@ namespace MOBIS.Views
                 this.Papers.Add(paper);
             }
             CrossLocalNotifications.Current.Show("Nove zpravy z vasi oblibene firmy", "hura! :)");
-        }
-
-        void cist_vice(object sender, EventArgs args)
-        {
-            Navigation.PushAsync(new Reader(((Button)sender).CommandParameter.ToString()));
         }
     }
 }
