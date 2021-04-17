6440552f916cab390f56d9fb5dcd361ee0c8cd33
kwimbik <kwimbiks@gmail.com>
2021-04-17 03:47:40 +0200

    Update Reader.xaml.cs

--- MOBIS/MOBIS/Views/Reader.xaml.cs
+++ MOBIS/MOBIS/Views/Reader.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MOBIS.API;
+using MOBIS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,11 +15,20 @@ namespace MOBIS.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Reader : ContentPage
     {
-        public Reader()
+        public Reader(string id)
         {
             InitializeComponent();
+            read_message(id);
             this.BindingContext = new ViewModels.UcetViewModel();
-            Zprava.Text = " ";
+        }
+
+      
+        private void read_message(string id)
+        {
+            string jsonInData = "{ \"Id\":" + id + "}";
+            string jsonOutData = RestApi.Post("content/load", jsonInData, out bool ok); 
+            var data = JsonSerializer.Deserialize<Paper>(jsonOutData);
+            Zprava.Text = data.ContentText;
         }
     }
 }
