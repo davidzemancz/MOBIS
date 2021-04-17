f0e5c988f10e3bd11b2092a5710ef35236a913de
kwimbik <kwimbiks@gmail.com>
2021-04-17 09:25:50 +0200

    Update Reader.xaml.cs

--- MOBIS/MOBIS/Views/Reader.xaml.cs
+++ MOBIS/MOBIS/Views/Reader.xaml.cs
@@ -22,7 +22,26 @@ namespace MOBIS.Views
             this.BindingContext = new ViewModels.UcetViewModel();
         }
 
-      
+        public Reader(string id, bool edit)
+        {
+            InitializeComponent();
+            edit_message(id);
+            Save_button.IsVisible = true;
+            Save_button.IsEnabled = true;
+            this.BindingContext = new ViewModels.UcetViewModel();
+        }
+
+        private void edit_message(string id)
+        {
+            string jsonInData = "{ \"Id\":" + id + "}";
+            string jsonOutData = RestApi.Post("content/load", jsonInData, out bool ok);
+            var data = JsonSerializer.Deserialize<Paper>(jsonOutData);
+            Editovatelna_zprava.Text = data.ContentText;
+            Editovatelna_zprava.IsVisible = true;
+            Title = data.Date + " " + data.Title;
+        }
+
+
         private void read_message(string id)
         {
             string jsonInData = "{ \"Id\":" + id + "}";
@@ -30,6 +49,11 @@ namespace MOBIS.Views
             var data = JsonSerializer.Deserialize<Paper>(jsonOutData);
             Zprava.Text = data.ContentText;
             Title = data.Date + " " + data.Title;
+        }
+
+        private void save_message(object sender, EventArgs args)
+        {
+            //poslat zpatky text s id zpravy na server ktery ji pushne ostatnim modifikovanou
         }
     }
 }
