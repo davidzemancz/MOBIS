c589a8712716e1507a4f06694f919e2313091049
kwimbik <kwimbiks@gmail.com>
2021-04-17 12:45:14 +0200

    Create new_message_form.xaml.cs

---
+++ MOBIS/MOBIS/Views/new_message_form.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOBIS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class new_message_form : ContentPage
+    {
+        public new_message_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
